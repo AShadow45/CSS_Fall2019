546a11fdbeb407317c660b342bc288eed00f7e43
Analisse Guadeloupe <31444172+AShadow45@users.noreply.github.com>
2019-10-24 14:54:22 -0400

    Add files via upload

---
+++ AnimHash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimHash
+{
+    public static readonly int HealerWalk = Animator.StringToHash("HealerIsWalk");
+    public static readonly int HealerHeal = Animator.StringToHash("IsHealing");
+    public static readonly int HealerDie = Animator.StringToHash("HealerDie");
+
+
+    public static readonly int WitchWalk = Animator.StringToHash("WitchIsWalk");
+    public static readonly int WitchDie = Animator.StringToHash("WitchDie");
+    //public static readonly int WitchWalk = Animator.StringToHash("WitchIsWalk");
+
+    public static readonly int KnightWalk = Animator.StringToHash("KnightIsWalk");
+    public static readonly int KnighAttack = Animator.StringToHash("KnightIsAttack");
+    public static readonly int KnightDie = Animator.StringToHash("KnightDie");
+
+    public static readonly int SuperheroWalk = Animator.StringToHash("SuperIsWalk");
+    public static readonly int SuperheroAttack = Animator.StringToHash("SuperIsAttack");
+    public static readonly int SuperheroDie = Animator.StringToHash("SuperDie");
+    public static readonly int SuperheroPushBox = Animator.StringToHash("IsPushBox");
+}
---
+++ movingPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 velocity;
+
+    private bool moving;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            moving = true;
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+}
